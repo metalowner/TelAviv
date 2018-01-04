bfb3de09de7588d8abe1046d00ab6dda6b3b284d
Pavel Yudkevich <29350384+metalowner@users.noreply.github.com>
2018-01-04 08:00:38 +0200

    commit 1
    initial alfa

---
+++ data.js
@@ -0,0 +1,49 @@
+var map;
+
+var officeLocations = [
+	{
+         title: 'Amazing 3 bedrooms near the beach',
+         location: {
+             lat: 32.07426,
+             lng: 34.767958
+         },
+         price: 1000,
+         type: 'apartment'
+     },
+     {
+         title: 'Cozy 2 bedrooms apartment',
+         location: {
+             lat: 32.07326,
+             lng: 34.765958
+         },
+         price: 1400,
+         type: 'apartment'
+     },
+     {
+         title: 'Beautiful office at the center',
+         location: {
+             lat: 32.07480,
+             lng: 34.778558
+         },
+         price: 1000,
+         type: 'office'
+     },
+     {
+         title: 'Fully furnished bedroom for short terms',
+         location: {
+             lat: 32.07480,
+             lng: 34.767998
+         },
+         price: 2000,
+         type: 'apartment'
+     },
+     {
+         title: 'New 3 bedrooms apartment',
+         location: {
+             lat: 32.07445,
+             lng: 34.777965
+         },
+         price: 1000,
+         type: 'apartment'
+     }
+];
---
+++ viewmodel.js
@@ -0,0 +1,38 @@
+function property(data) {
+	var self = this;
+
+	this.description = '<span>' + data.title + '</span>' +
+		'<span>' + data.price + '</span>' +
+		'<span>' + data.type + '</span>';
+
+	this.descriptionWindow = new google.maps.InfoWindow({
+		content: this.description
+	});
+
+	this.marker = new google.maps.Marker({
+		map: map,
+		animation: google.maps.Animation.DROP,
+		position: data.location
+	});
+
+	this.marker.addListener('click', function() {
+		descriptionWindow.open(map, marker);
+	});
+}
+
+function viewModel() {
+	var self = this;
+
+	this.properties = ko.observableArray([]);
+
+	map = new google.maps.Map(document.getElementById('googleMap'), {
+		center: {lat: 32.07426, lng: 34.767958},
+		zoom: 14
+	});
+
+	officeLocations.forEach(function(newProperty) {
+		this.properties.push(new property(newProperty));
+	});
+}
+
+ko.applyBindings(new viewModel());

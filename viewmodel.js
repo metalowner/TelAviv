da3608976fa5ffaa58c211fcee93b23906443714
Pavel Yudkevich <29350384+metalowner@users.noreply.github.com>
2018-01-08 17:01:58 +0200

    4th commit
    updated: lacks errorhandling

--- viewmodel.js
+++ viewmodel.js
@@ -4,36 +4,41 @@ function property(data) {
 	this.title = data.title;
 	this.type = data.type;
 	this.location = data.location;
+	this.price = data.price;
 	this.lat = data.location.lat;
 	this.lng = data.location.lng;
 	this.position = this.lat + ',' + this.lng;
 	this.pictures = data.pictures;
+	this.description = data.description;
 
 	this.contact = '<h3>Contact</h3><br><span>Igale 054-3440022</span><br>' +
 	'<span>Pavel 054-5544471</span><br><span>Email: telaviv@exclusive26.com</span><br>';
 	
-	this.descriptionInfo = '<h2>' + data.title + '</h2><hr><br>' +
-		'<img class="imgItem" src="images/' + data.pictures[0] + '" />' +
-		'<img class="imgItem" src="images/' + data.pictures[1] + '" /><br>' +
-		'<span>Price: ' + data.price + ' Ils / month</span><br>' +
-		'<span>Type: ' + data.type + '</span><br>' + this.contact +
-		'<h3 class="nearestParkings">Nearest Parkings (Ordered by Foursquare API)</h3><br>';
+	this.descriptionInfo = '<h2>' + this.title + '</h2><hr><br>' +
+		'<span><h3>Description</h3> ' + this.description + '</span><hr><br>' +
+		'<img class="imgItem" src="images/' + this.pictures[0] + '" />' +
+		'<img class="imgItem" src="images/' + this.pictures[1] + '" /><br>' +
+		'<span>Price: ' + this.price + ' Ils / month</span><br>' +
+		this.contact +
+		'<h3 class="nearestRestaurants">Nearest Restaurants (Ordered by Foursquare API)</h3>';
 	
-	this.parkings = ko.observableArray([]);
+	this.restaurants = ko.observableArray([]);
 
 	var foursquareRequest = 'https://api.foursquare.com/v2/venues/search' +
 		'?client_id=0NZGCGSS4CE1B5TDRJLXIIGI5UN5K0TSSLBO3GT215JBUVMQ' +
 		'&client_secret=SLAYSZONNPEMU2LQD0WCMUKXUXCB3QLCUEGCN1ATGDONUBMP' +
-		'&ll=' + this.position + '&query=parking' + '&v=20160118';
+		'&ll=' + this.position + '&query=restaurant' + '&v=20160118';
 
 	$.getJSON(foursquareRequest).done(function(data) {
 		this.temParkings = data.response.venues;
 		for (var i = 0; i < 5; i++) {
 			var parkingArr = this.temParkings[i];
 			this.name = parkingArr.name;
-			self.parkings.push('<span>' + this.name + '</span><br>');
+			this.address = parkingArr.location.formattedAddress;
+			this.popularity = parkingArr.stats.checkinsCount;
+			self.restaurants.push('<h4>' + this.name + '</h4><span>Popularity:<b> ' + this.popularity + ' visited here.</b></span><br><span>Address: ' + this.address + '</span><br>');
 		}
-		descriptionWindow.setContent(self.descriptionInfo += self.parkings.slice());
+		descriptionWindow.setContent(self.descriptionInfo += self.restaurants.slice());
 	});
 
 	var descriptionWindow = new google.maps.InfoWindow({
@@ -86,12 +91,14 @@ function viewModel() {
 		var searchTerm = self.searchTerm().toLowerCase();
 
 		if (filterType === "all" && !searchTerm) {
+			self.searchTerm().value = "";
 			$('#searchInput').hide();
 			var temporaryProperties = self.properties.slice();
 
 			return temporaryProperties.filter(function(property) {
 				if (property.type != undefined) {
 					property.marker.setMap(map);
+					map.fitBounds(bounds);
 				}
 				return property.type;
 			});
